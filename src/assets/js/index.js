674ac8fab9ae2f2cc204f96f3fd988246a19ba14
Ronnasayd <ronnasayd@hotmail.com>
2020-12-19 19:26:37 -0300

    refactor: add on method

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,18 +1,22 @@
-const $ = string => {
+const $$ = string => {
   const elements = [...document.querySelectorAll(string)]
+  if (elements) {
+    elements.forEach(element => {
+      element.on = (string, callback) => {
+        element.addEventListener(string, callback, false)
+      }
+    })
+  }
+
   return elements.length === 1 ? elements[0] : elements
 }
 
 let showDropMenu = false
-const dropMenu = $('#dropdown-menu')
-const navContent = $('.menu__content__nav')
+const dropMenu = $$('#dropdown-menu')
+const navContent = $$('.menu__content__nav')
 
-navContent.addEventListener(
-  'click',
-  event => {
-    showDropMenu = !showDropMenu
-    dropMenu.setAttribute('display', showDropMenu)
-  },
-  false
-)
+navContent.on('click', event => {
+  showDropMenu = !showDropMenu
+  dropMenu.setAttribute('display', showDropMenu)
+})
 // console.log(dropMenu)
